1f1a518896a3690b66beb0ecef12451803566c3b
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-08 06:29:11 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,9 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+// Routing with login authentication
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
